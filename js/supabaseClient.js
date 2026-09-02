91526d85ecf842849337e8970627ea1900f01c0a
bullestudiocompany-prog <bullestudiocompany@gmail.com>
2026-09-02 22:42:30 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const supabaseUrl = 'https://bvehhmbowizwsqrbdnwu.supabase.co/rest/v1/'; // Remplace par ton URL Supabase
+const supabaseUrl = 'https://bvehhmbowizwsqrbdnwu.supabase.co'; // Remplace par ton URL Supabase
 const supabaseAnonKey = 'sb_publishable_B4I-hDnB8ic_IRa7ZRIRMQ_zGrteY2Q';           // Remplace par ta clé anon public
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
