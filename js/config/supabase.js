26a039fce70ba251136ce47de96f82fd0bf8d05b
bullestudiocompany-prog <bullestudiocompany@gmail.com>
2026-09-02 11:19:37 +0100

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,1 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+// Remplace ces deux textes par l'URL et la clé ANON de ton projet Supabase
+const SUPABASE_URL = 'https://bvehhmbowizwsqrbdnwu.supabase.co/rest/v1/
+
+'
+const SUPABASE_KEY = 'sb_publishable_B4I-hDnB8ic_IRa7ZRIRMQ_zGrteY2Q'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
