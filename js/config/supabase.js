cc6f38933fb20b75d92bb30435c0e823c4057c7a
bullestudiocompany-prog <bullestudiocompany@gmail.com>
2026-09-03 09:02:40 +0100

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/+esm';
 
 const SUPABASE_URL = 'https://bvehhmbowizwsqrbdnwu.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_B4I-hDnB8ic_IRa7ZRIRMQ_zGrteY2Q';
